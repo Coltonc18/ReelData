3e36ac123da59336f39a2f906cb4f76e7b970ba6
Colton Carroll <coltonc18@icloud.com>
2023-04-17 19:53:50 -0700

    Created graphs.py

---
+++ graphs.py
@@ -0,0 +1,12 @@
+class Graphs:
+    def __init__(self, *args):
+        if 'xyz' in args:
+            self.graph_xyz()
+        elif 'abc' in args:
+            self.graph_abc()
+
+    def graph_xyz(self):
+        pass
+
+    def graph_abc(self):
+        pass
