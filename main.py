aaa431a03050377ebd1de8a487376ca4785dbd94
Colton Carroll <coltonc18@icloud.com>
2023-04-29 09:46:40 -0700

    Convert JSON columns

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import time
+import json
+import re
 
 import numpy as np
 import pandas as pd
@@ -8,7 +10,26 @@ from graphs import Graphs
 
 
 def main():
-    merge_data()
+    # merge_data()
+
+    metadata_df = pd.read_csv('data/movies_metadata.csv', low_memory=False)
+    # Remove rows that are not movies
+    metadata_df = metadata_df[metadata_df['video'] == False]
+    metadata_df = metadata_df[metadata_df['status'] == 'Released']
+    metadata_df['id'] = metadata_df['id'].astype('int32')
+    metadata_df.drop(['adult', 'belongs_to_collection', 'homepage', 'overview', 'poster_path', 'status', 
+                      'tagline', 'Unnamed: 0'], axis='columns', inplace=True)
+    # for column in ['genres', 'production_companies', 'production_countries', 'spoken_languages']:
+    #     metadata_df[column] = metadata_df[column].apply(json_to_columns, args=('name',))
+    #     print(metadata_df[column])
+    
+    # Converts a stringified JSON into a comma separated list, which will be parsed later
+    metadata_df['production_companies'] = metadata_df['production_companies'].apply(json_to_columns, args=('name',))
+    print(metadata_df['production_companies'])
+
+    # df = pd.read_csv('data/master_dataset.csv')
+    # print(df.columns)
+
     # graph = Graphs()
     # graph.create_graphs('example')
     pass
@@ -33,8 +54,11 @@ def merge_data(verbose=False):
     print(f'Ratings average df has data for {len(rating_avg_df)} movies') if verbose else None
 
     metadata_df = pd.read_csv('data/movies_metadata.csv', low_memory=False)
+    # Remove rows that are not movies
+    metadata_df = metadata_df[metadata_df['video'] == False]
+    metadata_df = metadata_df[metadata_df['status'] == 'Released']
     metadata_df['id'] = metadata_df['id'].astype('int32')
-    metadata_df.drop(['belongs_to_collection', 'homepage', 'overview', 'poster_path', 'status', 
+    metadata_df.drop(['adult', 'belongs_to_collection', 'homepage', 'overview', 'poster_path', 'status', 
                       'spoken_languages', 'tagline', 'Unnamed: 0'], axis='columns', inplace=True)
     
     # RT Critic Ratings
@@ -66,7 +90,7 @@ def merge_data(verbose=False):
     # Reorder the columns and filter out a few
     master_df = master_df.loc[:, ['id', 'imdb_id', 'rotten_tomatoes_link', 'title', 'budget', 'revenue', 
                                   'review_score', 'review_type', 'release_date', 'streaming_release_date', 
-                                  'adult', 'runtime', 'user_rating', 'vote_average', 'vote_count', 
+                                  'runtime', 'user_rating', 'vote_average', 'vote_count', 
                                   'original_language', 'popularity', 'production_companies', 
                                   'production_countries', 'directors', 'authors', 'actors', 'cast', 
                                   'tomatometer_status', 'tomatometer_rating', 'tomatometer_count', 
@@ -81,7 +105,7 @@ def merge_data(verbose=False):
 '''
 Takes a row from the rotten_tomatoes_critic_reviews.csv dataset and converts the expert ratings from
 fraction or letter value into a score out of 100 points. If there is no value, but only a "Fresh" or
-"Rotten" label, the value is set to 80/100 and 40/100, respectively
+"Rotten" label, the value is set to 80/100 and 40/100 respectively.
 '''
 def _convert_ratings(row):
     # Pull the rating value out of the column
@@ -111,6 +135,42 @@ def _convert_ratings(row):
     
     # If nothing else, the value is already set, and will be returned as is
     return float(value)
+
+'''
+Method is intended to be used in a DataFrame.apply(json_to_columns) application.
+
+Takes a cell from the DataFrame and converts the stringified JSON in that cell to a comma
+separated list of values as defined by the parameter "key", which is then returned
+'''
+def json_to_columns(cell, key):
+    try:
+        # Use regex look aheads and look behinds to avoid replacing apostrophes in the middle of
+        # a word with a double quotation, which would mess up the json parsing
+        cell = re.sub("(?<=[A-Za-z])'(?=[A-Za-z])", "<apostrophe>", cell)
+
+        # Replace all single quotes with double quotes
+        cell = cell.replace("'", '"')
+
+        # Turn the apostophes back into single quotes
+        cell = cell.replace("<apostrophe>", "'")
+
+        # Load the json string into a list of dictionaries
+        json_obj = json.loads(cell)
+        values = ''
+
+        # Iterate through each list index and convert the json format into a comma separated list
+        for item in json_obj:
+            values += f'{item[key]}*'
+        values = values[:-1].replace('*', ', ')
+
+        # Values are returned in the format "item1, item2, item3, ..."
+        return values
+    
+    except json.decoder.JSONDecodeError:
+        # In certain cases, there are apostrophes that cannot be caught with the regex expression above,
+        # which causes the JSON parser to throw an error as the strings aren't closed properly. In that case,
+        # just return an empty string
+        return ''
 
 '''
 DEPRECATED: Method to test the time difference between opening a csv file vs a gzipped csv file
