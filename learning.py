59724ef3f578455558e5080205b95204ab98d9f4
Colton Carroll <coltonc18@icloud.com>
2023-05-02 23:51:29 -0700

    Implementing create_learning_datasets: MEMORY

--- learning.py
+++ learning.py
@@ -1,12 +1,62 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
+import time
+import sys
+import datetime
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeRegressor
 
 sns.set()
+
+def create_learning_dataset():
+    # Read in and filter the master dataset to the desired columns for Machine Learning
+    df = pd.read_csv('data/master_dataset.csv').loc[:4500]
+    df = df.loc[:, ['content_rating', 'budget', 'revenue', 'calc_RT_rating', 'RT_expert_class', 'release_date', 
+                                  'runtime', 'user_rating', 'genres', 'original_language', 'production_companies', 
+                                  'production_countries', 'directors', 'authors', 'cast', 'tomatometer_status', 'RT_expert_rating', 
+                                  'tomatometer_count', 'audience_status', 'audience_rating', 'audience_count', 'tomatometer_fresh_critics_count', 
+                                  'tomatometer_rotten_critics_count']]
+    
+
+    # Convert tomatometer_fresh/rotten_critics_count to percentages
+    total_counts = df['tomatometer_rotten_critics_count'] + df['tomatometer_fresh_critics_count']
+    df['tomatometer_fresh_percentage'] = df['tomatometer_fresh_critics_count'] / total_counts * 100
+    df['tomatometer_rotten_percentage'] = df['tomatometer_rotten_critics_count'] / total_counts * 100
+    # Remove the old columns, as they will likely confuse the model
+    df.drop(['tomatometer_fresh_critics_count', 'tomatometer_rotten_critics_count'], axis='columns', inplace=True)
+
+    # Convert each actor, author, etc into their own column with manual One-Hot-Encoding
+    encoded_df = pd.DataFrame(index=range(len(df)))
+    for column in ['authors', 'directors', 'cast', 'genres', 'production_companies', 'production_countries']:
+        # Convert NaN values to empty strings to avoid type errors
+        df[column] = df[column].fillna('')
+
+        # Take each column (Series) and apply a lambda function to it, which returns a new Series of ones with the index as the name
+        # Then replace any NaN values with 0 and cast the Series to an int0 (0 or 1 only) which should use the lowest memory
+        # The list(set(x.split(', '))) portion converts the comma separated list (string) into a list, and removes any duplicates
+        encoded_column = df[column].apply(lambda x: pd.Series([1] * len(list(set(x.split(', ')))), index=list(set(x.split(', '))))).fillna(0, downcast='infer').astype(np.int0) # type: ignore
+        # Add a prefix to the name so that it is clear which column the name came from
+        encoded_column = encoded_column.add_prefix(f'{column}_')
+        # Append the encoded column to the DataFrame
+        encoded_df = pd.concat([encoded_df, encoded_column], axis=1)
+    
+    # Convert the release date from a string to datetime type
+    df['release_date'] = pd.to_datetime(df['release_date'], format='%Y-%m-%d')
+    # Dates will be stored as an ordinal (time since the year zero)
+    df['release_date'] = df['release_date'].apply(lambda date: datetime.date.toordinal(date))
+
+    # Drop the columns that have already been manually encoded
+    df = df.drop(['authors', 'directors', 'cast', 'genres', 'production_companies', 'production_countries'], axis='columns')
+    # Complete the One-Hot-Encoding on the rest of the classification columns and concatenate the manually encoded ones
+    df = pd.concat([pd.get_dummies(df), encoded_df], axis=1)
+
+    # Save to a csv file
+    df.to_csv('data/learning.csv', index=False)
+
 
 def regressive_model(label_column, error=0.2):
     master_df = pd.read_csv('data/master_dataset.csv')
@@ -83,4 +133,11 @@ def neural_network():
     pass
 
 if __name__ == '__main__':
-    regressive_model('revenue', error=0.1)
+    t1 = time.time()
+    df = pd.read_csv('data/learning.csv')
+    print(f'Took {time.time() - t1} seconds to open CSV')
+    print(f'Size in RAM is {sys.getsizeof(df)}')
+    
+
+    # create_learning_dataset()
+    # regressive_model('revenue', error=0.1)
